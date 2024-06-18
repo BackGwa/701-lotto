12f0890373f3882dd672d7d70105dc420e38d4ad
강찬영 <backgwa@icloud.com>
2024-06-19 00:03:41 +0900

    데이터 구조 개선
    기본 joined 제거

--- scripts/clan_player.js
+++ scripts/clan_player.js
@@ -1,47 +1,17 @@
 let player_list = {
-    "왈라비" : {
-        joined : false
-    },
-    "그니그니" : {
-        joined : false
-    },
-    "김현민" : {
-        joined : false
-    },
-    "뀨으뀨유" : {
-        joined : false
-    },
-    "백과" : {
-        joined : false
-    },
-    "희진" : {
-        joined : false
-    },
-    "초보" : {
-        joined : false
-    },
-    "히히" : {
-        joined : false
-    },
-    "도로동동" : {
-        joined : false
-    },
-    "시온" : {
-        joined : false
-    },
-    "유치" : {
-        joined : false
-    },
-    "임시훈" : {
-        joined : false
-    },
-    "히창" : {
-        joined : false
-    },
-    "nursing2170" : {
-        joined : false
-    },
-    "merai33" : {
-        joined : false
-    }
+    "왈라비" : {},
+    "그니그니" : {},
+    "김현민" : {},
+    "뀨으뀨유" : {},
+    "백과" : {},
+    "희진" : {},
+    "초보" : {},
+    "히히" : {},
+    "도로동동" : {},
+    "시온" : {},
+    "유치" : {},
+    "임시훈" : {},
+    "히창" : {},
+    "nursing2170" : {},
+    "merai33" : {}
 }
