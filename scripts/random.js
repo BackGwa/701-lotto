84405445cd29331fd7ced822ce05c47ed8469a90
강찬영 <backgwa@icloud.com>
2024-06-18 23:42:37 +0900

    안전한 랜덤 방식 사용
    기존 방식에서 더욱 안전한 랜덤 알고리즘 사용

--- scripts/random.js
+++ scripts/random.js
@@ -1,3 +1,15 @@
 function randint(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    const range = max - min + 1;
+    const randomBytes = new Uint32Array(1);
+    let randomValue;
+    
+    do {
+        window.crypto.getRandomValues(randomBytes);
+        randomValue = randomBytes[0] / (0xFFFFFFFF + 1);
+    } while (randomValue * range >= range);
+
+    return Math.floor(randomValue * range) + min;
 }
